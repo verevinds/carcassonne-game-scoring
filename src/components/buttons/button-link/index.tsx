e987e15cc02c03366653e8cd74096763ac3f839b
Denis Verevin <verevin@mercurydevelopment.com>
2023-12-25 11:14:38 +0100

    Refactor button-link component and remove picker-modal component

--- src/components/buttons/button-link/index.tsx
+++ src/components/buttons/button-link/index.tsx
@@ -1,18 +1,16 @@
 import * as Haptics from 'expo-haptics';
 import { Link } from 'expo-router';
-import { LinkProps } from 'expo-router/build/link/Link';
-import { View, Text, GestureResponderEvent } from 'react-native';
+import { View, Text } from 'react-native';
 
 import ArrowStick from 'assets/icons/arrow-stick';
 import Hint from 'components/hint';
 
 import messages from './index.messages';
 import { styles } from './index.styles';
+import { ButtonLinkProps, ReactMouseEvent } from './index.types';
 
-const ButtonLink = (props: LinkProps & { hint?: string }): JSX.Element => {
-  function handlePress(
-    e: React.MouseEvent<HTMLAnchorElement, MouseEvent> | GestureResponderEvent,
-  ) {
+const ButtonLink = (props: ButtonLinkProps): JSX.Element => {
+  function handlePress(e: ReactMouseEvent) {
     Haptics.notificationAsync(Haptics.NotificationFeedbackType.Success);
     props.onPress?.(e);
   }
